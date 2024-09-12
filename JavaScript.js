9614a0bb2a1b0d94a1433a085ad4addcb481975d
Praise Bucuzong Bah <87252492+Praisebah@users.noreply.github.com>
2024-09-12 17:26:39 +0100

    DOM element sections (by Praise)

---
+++ JavaScript.js
@@ -0,0 +1,10 @@
+// Existing DOM element selections
+const container = document.querySelector('.container');
+const searchBox = document.querySelector('.search-box');
+const searchButton = document.querySelector('.search-box button');
+const weatherBox = document.querySelector('.weather-box');
+const weatherDetails = document.querySelector('.weather-details');
+const error404 = document.querySelector('.not-found');
+const cityHide = document.querySelector('.city-hide');
+const logo = document.getElementById('logo');
+const activityButtons = document.querySelector('.activity-buttons');
